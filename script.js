9eb81fa53c1b1ed9dcf2d1afa7deefa1d0034175
Danilo Silva <danilo.silva@domvsit.com.br>
2022-01-10 23:27:58 -0300

    Finalizando projeto

--- script.js
+++ script.js
@@ -1,27 +1,84 @@
+function chooseSelectTo() {
+  var selectFrom = document.getElementById('selectFrom').value;
+
+  if(selectFrom == 'binario') {
+    document.getElementById('title').innerHTML = 'Conversor Bin2Dec' 
+    document.getElementById('selectTo').innerHTML = 'Decimal' 
+    document.getElementById('inputSpan').innerHTML = '2'
+    document.getElementById('outputSpan').innerHTML = '10'
+  } else {
+    document.getElementById('title').innerHTML = 'Conversor Dec2Bin' 
+    document.getElementById('selectTo').innerHTML = 'Binario'
+    document.getElementById('inputSpan').innerHTML = '10'
+    document.getElementById('outputSpan').innerHTML = '2'
+  } 
+
+  document.getElementById('input').value = ''
+  document.getElementById('output').value = ''
+}
+
 function calculate() {
-  var binValue = document.getElementById('binValue').value;
-  binValue = binValue.split('').reverse().join('');
+  var select = document.getElementById('selectFrom').value;
+
+  if(select == 'binario') 
+    bin2dec()
+  else
+    dec2bin()
+}
+
+function bin2dec() {
+  var input = document.getElementById('input').value;
+  input = input.split('').reverse().join('');
 
   const validateStr = new RegExp(/^[0-1]+$/);
-  const isValid = validateStr.test(binValue);
+  const isValid = validateStr.test(input);
   
   if(!isValid) {
     alert("Input aceita apenas 0 e 1");
-    document.getElementById('decValue').value = '';
+    document.getElementById('output').value = '';
     return;
   } 
 
-  let length = binValue.length;
+  let length = input.length;
   let count = 0;
-  let decValue = 0;
+  let output = 0;
 
   do{
-    decValue += binValue[count] * Math.pow(2, count)    
+    output += input[count] * Math.pow(2, count)    
     count ++;
   } while(count < length)
 
-  if(decValue) 
-    document.getElementById('decValue').value = decValue;
+  if(output) 
+    document.getElementById('output').value = output;
   else 
-    document.getElementById('decValue').value = ''
+    document.getElementById('output').value = ''
+}
+
+function dec2bin() {
+  var input = document.getElementById('input').value;
+
+  const validateStr = new RegExp(/^[0-9]+$/);
+  const isValid = validateStr.test(input);
+  
+  if(!isValid) {
+    alert("Input aceita apenas numero");
+    document.getElementById('output').value = '';
+    return;
+  } 
+  
+  let output = 0;
+  let rem = 0;
+  let i = 1;
+
+  while (input != 0) {
+    rem = input % 2;
+    input = parseInt(input / 2);
+    output = output + rem * i;
+    i = i * 10;
+  }
+
+  if(output || output == 0) 
+    document.getElementById('output').value = output;
+  else 
+    document.getElementById('output').value = ''
 }
